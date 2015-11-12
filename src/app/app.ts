72b43bc12e44c618369af7a698d27f6cc68c43ff
joaogarin <joao.garin@gmail.com>
2015-11-12 09:18:48 +0100

    refactoring

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,12 @@
 /*
  * Angular 2 decorators and services
  */
-import {Directive, Component, View, ElementRef} from 'angular2/angular2';
-import {RouteConfig, Router} from 'angular2/router';
-import {Http, Headers} from 'angular2/http';
+import {Component, View} from 'angular2/angular2';
 
 /*
  * Angular Directives
  */
-import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {CORE_DIRECTIVES} from 'angular2/angular2';
 
 /*
 * Custom components
@@ -22,27 +19,17 @@ import {Logo} from "./components/logo/logo";
  */
 @Component({
     // The selector is what angular internally uses
-    // for `document.querySelectorAll(selector)` in our index.html
-    // where, in this case, selector is the string 'app'
     selector: 'app', // <app></app>
     // We need to tell Angular's compiler which directives are in our template.
     // Doing so will allow Angular to attach our behavior to an element
-    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, ROUTER_DIRECTIVES, Logo],
-    // Our list of styles in our component. We may add more to compose many styles together
+    directives: [CORE_DIRECTIVES, Logo],
+    // The template for our app
     template: `
         <logo></logo>
     `
 })
 export class App {
-    // These are member type
-    title:string;
-    // TypeScript public modifiers
-    constructor(public http:Http) {
-        this.title = 'Angular 2';
-    }
-
     onInit() {
         // Our API
     }
-
 }
