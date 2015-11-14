79f5e2a77958ac037faf871385f3bbcc76a99d19
joaogarin <joao.garin@gmail.com>
2015-11-14 23:51:33 +0100

    Added scoped selectors

--- src/app/components/ScopedSelectors/ScopedSelectorsDemo.ts
+++ src/app/components/ScopedSelectors/ScopedSelectorsDemo.ts
@@ -5,16 +5,24 @@ import {Component, View} from 'angular2/angular2';
 import {Snippet} from "../shared/Snippet/Snippet";
 
 var styles = require("./ScopedSelectors.css");
+var js = require('!!raw!./ScopedSelectors.ts');
+var css = require('!!raw!./ScopedSelectors.css');
 
 @Component({
     directives: [Snippet],
     'selector': 'scoped-selectors-demo',
-    'template': `<snippet [innertext]="innerContent"></snippet>`
+    'template': `<snippet [files]="files" [innertext]="innerContent"></snippet>`
 })
 export class ScopedSelectorsDemo {
-    innerContent: any;
+    innerContent: string;
+    files: any;
 
     onInit(){
+        this.files = [
+            { name: 'ScopedSelectors.ts', source: js },
+            { name: 'ScopedSelectors.css', source: css }
+        ];
+
         this.innerContent = `<div class=${styles.root}>
             <p class=${styles.text}>Scoped Selectors</p>
           </div>`;
--- src/app/components/shared/Snippet/snippet.ts
+++ src/app/components/shared/Snippet/snippet.ts
@@ -1,19 +1,24 @@
 /*
  * Angular 2 decorators and services
  */
-import {Component, View} from 'angular2/angular2';
+import {Component, View, NgFor} from 'angular2/angular2';
 
 var styles = require("./snippet.css");
 
 @Component({
-    'inputs': ['innertext'],
+    'inputs': ['innertext','files'],
     'selector': 'snippet',
+    'directives': [NgFor],
     'template': `<div class=${styles.output}>
-          <div class=${styles.fileName}>Output</div>
-          <div class=${styles.outputContent} [inner-html]="innertext">
-          </div>
-        </div>`
+                  <div class=${styles.fileName}>Output</div>
+                  <div class=${styles.outputContent} [inner-html]="innertext"></div>
+                </div>
+                <div *ng-for="#file of files" class=${styles.file}>
+                  <div class=${styles.fileName}>{{ file.name }}</div>
+                  <pre class=${styles.pre}>{{ file.source }}</pre>
+                </div>`
 })
 export class Snippet {
-    innertext: any;
+    innertext: string;
+    files: any;
 }
