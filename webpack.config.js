a3d2bb12e39e935d57b482f982c818857d50e870
Vetere Remo <Remo.Vetere@axelspringer.ch>
2016-04-28 11:18:33 +0200

    Added support for sourceMap

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
     preLoaders: [ { test: /\.ts$/, loader: 'tslint-loader' } ],
     loaders: [
       //{ test: /\.css$/,   loader: 'raw' },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader?sourceMap') },
 
       // Support for .ts files.
       {
