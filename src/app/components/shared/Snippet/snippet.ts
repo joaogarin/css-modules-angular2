f64f87d9196a0fd5657aa6515fc02ebf48aa5d0b
Joao Garin <joao.garin@gmail.com>
2016-05-03 16:38:22 +0200

    use let in ngFor

--- src/app/components/shared/Snippet/snippet.ts
+++ src/app/components/shared/Snippet/snippet.ts
@@ -16,7 +16,7 @@ var styles = require('./snippet.css');
                     <ng-content></ng-content>
                   </div>
                 </div>
-                <div *ngFor="#file of files" class=${styles.file}>
+                <div *ngFor="let file of files" class=${styles.file}>
                   <div class=${styles.fileName}>{{ file.name }}</div>
                   <pre class=${styles.pre}>{{ file.source }}</pre>
                 </div>`
